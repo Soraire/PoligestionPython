74b47b2f271d117a39bb390dfbba42ae41995b12
Soraire <114581621+Soraire@users.noreply.github.com>
2023-10-30 10:11:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import ./Programs/decode.py
-import ./Programs/photoSave.py
-from ./Programs/face_recognition/recognition-app/detector.py import recognize_faces
+import './Programs/decode.py'
+import './Programs/photoSave.py'
+from './Programs/face_recognition/recognition-app/detector.py' import recognize_faces
 json=decode.py.run()
 dni = recognize_faces(
     "./Programs/imageToSave.png",
