cd12bf1452ccd4c4d0c8736258cddd9b7945bcf6
Soraire <114581621+Soraire@users.noreply.github.com>
2023-10-30 10:05:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ./Programs/photoSave.py
 from ./Programs/face_recognition/recognition-app/detector.py import recognize_faces
 json=decode.py.run()
 recognize_faces(
-    ./Programs/ImageToSave.png,
-    model: str = "hog",
+    ./Programs/imageToSave.png,
+     "hog",
     encodings_location: Path = DEFAULT_ENCODINGS_PATH,
 )
