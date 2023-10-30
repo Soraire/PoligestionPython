3eced5028b2d7708da9e7d237c7fdf85b21a0a63
Soraire <114581621+Soraire@users.noreply.github.com>
2023-10-30 10:08:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ./Programs/photoSave.py
 from ./Programs/face_recognition/recognition-app/detector.py import recognize_faces
 json=decode.py.run()
 recognize_faces(
-    ./Programs/imageToSave.png,
+    "./Programs/imageToSave.png",
      "hog",
-    encodings_location: Path = DEFAULT_ENCODINGS_PATH,
+    Path("Programs/face_recognition/recognition-app/output/encodings.pkl"),
 )
